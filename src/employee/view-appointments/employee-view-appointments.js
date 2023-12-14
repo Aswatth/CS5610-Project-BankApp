d7ef74a75f0b603254d4d82cb1c60b9d61b4410e
Aswatth <aswatth56@gmail.com>
2023-12-14 15:53:37 -0500

    bug fix

--- src/employee/view-appointments/employee-view-appointments.js
+++ src/employee/view-appointments/employee-view-appointments.js
@@ -200,10 +200,10 @@ export default function EmployeeViewAppointments({ hasCreateCusomterAccess }) {
       </Dialog>
       <DataTable
         value={
-          appointmentsToDisplay.filter((f) => f.customer.customerId != 0)
-            .length == 0
-            ? []
-            : appointmentsToDisplay.filter((f) => f.customer.customerId != 0)
+          appointmentsToDisplay
+            ? appointmentsToDisplay.filter((f) => f.customer.customerId != 0)
+                .length == 0
+            : []
         }
         scrollHeight="500px"
         selectionMode="single"
